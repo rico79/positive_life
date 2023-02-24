ec0ed84c85531ac372a541de12838b8638a37ce0
Eric Vogt <eric.vogt.pro@gmail.com>
2023-02-24 08:05:42 +0100

    link route instead of a

--- src/app.rs
+++ src/app.rs
@@ -14,18 +14,24 @@ fn switch(routes: Route) -> Html {
     match routes {
         Route::Home => html! { <h1>{ "Home" }</h1> },
         Route::NotFound => html! {
-            <div class="text-center">
-                <p class="text-base font-semibold text-indigo-600">
-                    { "404" }
-                </p>
-                <h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">{ "Page not found" }</h1>
-                <p class="mt-6 text-base leading-7 text-gray-600">{ "Sorry, we couldn’t find the page you’re looking for." }</p>
-                <div class="mt-10 flex items-center justify-center gap-x-6">
-                    <a href="#" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                        { "Go back home" }
-                    </a>
+            <main class="grid min-h-full place-items-center bg-white py-24 px-6 sm:py-32 lg:px-8">
+                <div class="text-center">
+                    <p class="text-base font-semibold text-indigo-600">
+                        { "404" }
+                    </p>
+                    <h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+                        { "Page not found" }
+                    </h1>
+                    <p class="mt-6 text-base leading-7 text-gray-600">
+                        { "Sorry, we couldn’t find the page you’re looking for." }
+                    </p>
+                    <div class="mt-10 flex items-center justify-center gap-x-6">
+                        <Link<Route> to={Route::Home} classes={"rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"}>
+                            { "Go back home" }
+                        </Link<Route>>
+                    </div>
                 </div>
-            </div>
+            </main>
         },
     }
 }
@@ -33,10 +39,8 @@ fn switch(routes: Route) -> Html {
 #[function_component(App)]
 pub fn app() -> Html {
     html! {
-        <main class="grid min-h-full place-items-center bg-white py-24 px-6 sm:py-32 lg:px-8">
-            <BrowserRouter>
-                <Switch<Route> render={switch} />
-            </BrowserRouter>
-        </main>
+        <BrowserRouter>
+            <Switch<Route> render={switch} />
+        </BrowserRouter>
     }
 }
