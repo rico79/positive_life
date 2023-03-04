737679a67c1ddc8ab5492c678d36dcbebeb9a9bc
Eric Vogt <eric.vogt.pro@gmail.com>
2023-03-04 18:36:04 +0100

    h8

--- src/app/navigation.rs
+++ src/app/navigation.rs
@@ -26,12 +26,12 @@ fn AppNavigationHeader() -> Html {
         <nav class="mx-auto flex max-w-7xl items-center justify-between p-6">
             <div class="flex flex-1">
                 <Link<Route> to={Route::Home} classes={"m-sm p-sm"} >
-                    <img class="h-10 w-auto" src="./icons/lotus-150.png" />
+                    <img class="h-8 w-auto" src="./icons/lotus-150.png" />
                 </Link<Route>>
             </div>
             <div class="flex flex-1 justify-end">
                 <Link<Route> to={Route::NotFound} classes={"m-sm p-sm"} >
-                    <img class="h-10 w-auto rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" />
+                    <img class="h-8 w-auto rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" />
                 </Link<Route>>
             </div>
         </nav>
